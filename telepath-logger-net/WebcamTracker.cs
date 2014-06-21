bca92d2d9bf428164e6b0175cc4d5184af718c7f
JoshVarty <joshvarty@gmail.com>
2014-06-21 17:39:08 -0400

    Initial commit of WebcamTracker

---
+++ telepath-logger-net/WebcamTracker.cs
@@ -0,0 +1,13 @@
+﻿
+namespace telepath_logger_net
+{
+    public class WebcamTracker
+    {
+        private int _intervalInMs;
+
+        public WebcamTracker(int intervalInMs = 60000)
+        {
+
+        }
+    }
+}
