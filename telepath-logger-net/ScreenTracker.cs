51a2b033f45cf885abd393fbf5a6a5a0dfae41b2
JoshVarty <joshvarty@gmail.com>
2014-06-21 15:54:18 -0400

    Added ScreenTracker
    We'll use this class to take screen captures of the user's screen.

---
+++ telepath-logger-net/ScreenTracker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telepath_logger_net
+{
+    /// <summary>
+    /// Periodically takes a screenshot of the user's desktop.
+    /// These screenshots are then saved, and can be later used 
+    /// to render a video.
+    /// </summary>
+    public class ScreenTracker
+    {
+
+        private int _intervalInMs;
+        public ScreenTracker(int intervalInMs = 1000)
+        {
+            _intervalInMs = intervalInMs;
+        }
+    }
+}
