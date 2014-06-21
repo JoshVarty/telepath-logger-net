292fc9d08f084e3404ed288ac8befa673289a4c2
JoshVarty <joshvarty@gmail.com>
2014-06-21 16:53:12 -0400

    Added utilities class
    Currently allows us to get the location of the running assembly. That's
    where I'll save the screenshots.

---
+++ telepath-logger-net/Utilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace telepath_logger_net
+{
+
+    public static class Utilities
+    {
+        /// <summary>
+        /// Returns the directory of the currently executing assembly.
+        /// See: http://stackoverflow.com/a/283917/300908
+        /// </summary>
+        static public string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+    }
+}
