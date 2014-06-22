5cf42db0d8471c3026b9faa99deedd66f35a86c1
JoshVarty <joshvarty@gmail.com>
2014-06-21 20:57:02 -0400

    Added Webcam WPF project I'm going to migrate to.
    It looks like something needs to be initialized correctly that only
    happens in WPF/Winforms. So I'll migrate to WPF instead of using the
    CommandLine project.

---
+++ WPFCSharpWebCam/Window1.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFCSharpWebCam
+{
+    /// <summary>
+    /// Design by Pongsakorn Poosankam
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        WebCam webcam;
+        private void mainWindow_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+            webcam = new WebCam();
+            webcam.InitializeWebCam(ref imgVideo);
+        }
+
+        private void bntStart_Click(object sender, RoutedEventArgs e)
+        {
+            
+            webcam.Start();
+        }
+
+        private void bntStop_Click(object sender, RoutedEventArgs e)
+        {
+            webcam.Stop();
+        }
+
+        private void bntContinue_Click(object sender, RoutedEventArgs e)
+        {
+            webcam.Continue();
+        }
+
+        private void bntCapture_Click(object sender, RoutedEventArgs e)
+        {
+            imgCapture.Source = imgVideo.Source;
+        }
+
+        private void bntSaveImage_Click(object sender, RoutedEventArgs e)
+        {
+            Helper.SaveImageCapture((BitmapSource)imgCapture.Source);
+        }
+
+        private void bntResolution_Click(object sender, RoutedEventArgs e)
+        {
+            webcam.ResolutionSetting();
+        }
+
+        private void bntSetting_Click(object sender, RoutedEventArgs e)
+        {
+            webcam.AdvanceSetting();
+        }
+    }
+}
